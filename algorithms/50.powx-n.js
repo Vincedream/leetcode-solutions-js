18f640b4c02ab5b1b6e9d9b9b27cdad29e737828
Vincedream <callmevincehua@gmail.com>
2020-03-29 16:47:10 +0800

    feat: 50.powx-n

---
+++ algorithms/50.powx-n.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=50 lang=javascript
+ *
+ * [50] Pow(x, n)
+ *
+ * https://leetcode.com/problems/powx-n/description/
+ *
+ * algorithms
+ * Medium (29.12%)
+ * Total Accepted:    419.9K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '2.00000\n10'
+ *
+ * Implement pow(x, n), which calculates x raised to the power n (x^n).
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 2.00000, 10
+ * Output: 1024.00000
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2.10000, 3
+ * Output: 9.26100
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: 2.00000, -2
+ * Output: 0.25000
+ * Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * -100.0 < x < 100.0
+ * n is a 32-bit signed integer, within the range [−2^31, 2^31 − 1]
+ * 
+ * 
+ */
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+function pow(x, n) {
+    console.log('2222')
+    if (n === 0) {
+        return 1
+    }
+    if (n === 1) {
+        return x
+    }
+    const subResult = pow(x, Math.floor( n / 2))
+    return n % 2 === 0 ? subResult * subResult : x *  subResult * subResult
+}
+
+var myPow = function(x, n) {
+    const powResult = pow(x, Math.abs(n))
+    return n < 0 ? 1 / powResult : powResult
+};
